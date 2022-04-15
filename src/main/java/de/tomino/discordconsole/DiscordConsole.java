a1ae1a4cc0a758474ca368d12bacee5cfe048bcf
TominoLP <erfinder.tom@gmail.com>
2022-04-15 20:22:45 +0200

    Initial commit

---
+++ src/main/java/de/tomino/discordconsole/DiscordConsole.java
@@ -0,0 +1,18 @@
+package de.tomino.discordconsole;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DiscordConsole extends JavaPlugin {
+
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
