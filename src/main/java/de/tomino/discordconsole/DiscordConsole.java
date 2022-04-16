9cd8cff92b171c5f811809fa69c8b168b51de928
TominoLP <erfinder.tom@gmail.com>
2022-04-16 04:51:30 +0200

    added Console Logger

--- src/main/java/de/tomino/discordconsole/DiscordConsole.java
+++ src/main/java/de/tomino/discordconsole/DiscordConsole.java
@@ -1,18 +1,64 @@
 package de.tomino.discordconsole;
 
+import de.tomino.discordconsole.utils.DiscordBot;
+import de.tomino.discordconsole.utils.Logger;
+import net.dv8tion.jda.api.JDA;
+import org.apache.logging.log4j.LogManager;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+import com.google.gson.Gson;
+
+import javax.security.auth.login.LoginException;
 
 public final class DiscordConsole extends JavaPlugin {
 
+    Gson gson = new Gson();
+
+
+    private static Plugin plugin;
+    private JDA jda;
+    private Logger appender;
 
     @Override
     public void onEnable() {
+
+
+
+
+        plugin = this;
+        Bukkit.getPluginManager().registerEvents(new Logger.CLogger(), this);
+
+        try {
+            DiscordBot.main();
+        } catch (LoginException e) {
+            throw new RuntimeException(e);
+        }
+
         // Plugin startup logic
+
+        appender = new Logger(this, this.jda);
+        try {
+            final org.apache.logging.log4j.core.Logger logger = (org.apache.logging.log4j.core.Logger) LogManager.getRootLogger();
+            logger.addAppender(appender);
+        } catch(Exception e) {
+
+        }
+
+
+
 
     }
 
+
+
+
     @Override
     public void onDisable() {
+        plugin = null;
+        final org.apache.logging.log4j.core.Logger logger = (org.apache.logging.log4j.core.Logger) LogManager.getRootLogger();
+        appender.stop();
+        logger.removeAppender(appender);
         // Plugin shutdown logic
     }
 }
---
+++ src/main/java/de/tomino/discordconsole/utils/Logger.java
@@ -0,0 +1,71 @@
+package de.tomino.discordconsole.utils;
+
+
+import de.tomino.discordconsole.DiscordConsole;
+import net.dv8tion.jda.api.JDA;
+import org.apache.logging.log4j.core.Filter;
+import org.apache.logging.log4j.core.Layout;
+import org.apache.logging.log4j.core.LogEvent;
+import org.apache.logging.log4j.core.appender.AbstractAppender;
+import org.apache.logging.log4j.core.config.Property;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.event.server.ServerCommandEvent;
+
+import java.io.Serializable;
+import java.util.Base64;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class Logger extends AbstractAppender {
+
+    private DiscordConsole plugin;
+    private JDA jda;
+    private String messages = "";
+    public static Calendar cal = Calendar.getInstance(Locale.GERMANY);
+    public static String time = String.format("%02d:%02d:%02d", cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), cal.get(Calendar.SECOND));
+
+    public Logger(DiscordConsole plugin, JDA j) {
+        super("MyAppender", null, null);
+        this.plugin = plugin;
+        this.jda = j;
+
+        start();
+    }
+
+
+    @Override
+    public void append(LogEvent event) {
+
+        String message = event.getMessage().getFormattedMessage();
+        message = "[" + time + " " + event.getLevel().toString() + "]: " + message + "\n";
+        DiscordBot.sendMessage(message);
+
+    }
+    public static class CLogger implements Listener {
+        @EventHandler
+        public void onServerCommand(ServerCommandEvent event) {
+            String command = event.getCommand();
+            command = "[" + time + " COM]: Console: /"  + command + "\n";
+            DiscordBot.sendMessage(command);
+        }
+
+        @EventHandler
+        public void onPlayerCommandPreprocess(PlayerCommandPreprocessEvent event) {
+            String command = event.getMessage();
+            command = "[" + time + " COM]: " + event.getPlayer().getName() + ": " + command + "\n";
+            DiscordBot.sendMessage(command);
+        }
+
+        @EventHandler
+        public void onAsyncPlayerChat(AsyncPlayerChatEvent event) {
+            String message = event.getMessage();
+            message = "[" + time + " MSG]: " + event.getPlayer().getName() + ": " + message + "\n";
+            DiscordBot.sendMessage(message);
+        }
+    }
+
+}
+
